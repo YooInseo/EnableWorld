b6a32628a0745a492a3602cb13a36aa7a253326a
BuildTools <unconfigured@null.spigotmc.org>
2022-07-31 05:20:50 +0900

    커밋 바이 SkyExcel

---
+++ src/main/java/skyexcel/Main/Main.java
@@ -0,0 +1,15 @@
+package skyexcel.Main;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static Main plugin;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
